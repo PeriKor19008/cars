490562846f8b3727d9f10a704e705bd26af5e680
peri <peri1908kor@gmail.com>
2022-10-17 17:24:51 +0300

    added bootstrap to some components (not all- maybe better way)

--- application/views/first_page.php
+++ application/views/first_page.php
@@ -1,16 +1,10 @@
-<?php $this->load->view("hf/header.html") ?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-<div>
-    <ol>
-        <a href="http://127.0.0.1:80/index.php/site/all_vehicles">Vehicles</a>
-    </ol>
-    <ol>
-        <a href="http://127.0.0.1:80/index.php/site/all_services">Services</a>
-    </ol>
-    <ol>
-        <a href="http://127.0.0.1:80/index.php/site/all_kteo">KTEO</a>
-    </ol>
-</div>
+<?php $this->load->view("navbar") ?>
+
+
 
 <?php $this->load->view("hf/footer.html") ?>
 
--- application/views/vehicles/all_vehicles.php
+++ application/views/vehicles/all_vehicles.php
@@ -1,14 +1,30 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
 <?php
+$this->load->view("navbar") ;
 
+echo "  <div class='panel-group'>";
 foreach ($cars as $car){
-    echo $car->license_plate ."<br>";
-    echo $car->manufacturer ."<br>";
-    echo $car->model ."<br>";
-    echo $car->color ."<br>";
-    $url="http://127.0.0.1:80/index.php/vehicles/car_details/".$car->id;
+    echo "<li class='list-group-item'>";
 
-    echo "<a href='$url'>details</a>";
-    echo "<br><br><br>";
+
+        echo $car->license_plate ."<br>" ;
+
+
+        echo $car->manufacturer ;
+        echo $car->model ."<br>";
+        echo $car->color ."<br>";
+        $url="http://127.0.0.1:80/index.php/vehicles/car_details/".$car->id;
+
+
+        echo "<a class='btn btn-primary btn-sm' href='$url'>details</a>";
+        echo "<br><br><br>";
+    echo"</li>";
 }
+echo "</div>";
+
 
 ?>
